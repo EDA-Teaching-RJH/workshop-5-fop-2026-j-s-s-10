0496d0ab14c7e0d383c33bbba677cf47395ae2d7
j-s-s-10 <jss10@kent.ac.uk>
2026-02-02 13:28:43 +0000

    Answered part one

--- camel.py
+++ camel.py
@@ -1,1 +1,11 @@
-# Enter your code here
+camel_case = input("Enter camel case variable name: ")
+len_camel_case = len(camel_case)
+
+snake_case = ""
+for i in range(len_camel_case):
+    if camel_case[i].isupper():
+        snake_case += f"_{camel_case[i].lower()}"
+    else:
+        snake_case += camel_case[i]
+
+print(snake_case)
